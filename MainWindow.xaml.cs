86c8406d3d88afdb7ea9e9d6bf20acc0713acc22
Mariana Licona <m.licona@abtis.de>
2021-06-21 01:33:41 +0200

    add mouse down event

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace MatchGame
         }
 
         private void SetUpGame()
-        {
+        {   /*This is a list of emojis*/
             List<string> animalEmoji = new List<string>()
             {
                 "🐱‍🐉", "🐱‍🐉",
@@ -47,6 +47,29 @@ namespace MatchGame
                 string nextEmoji = animalEmoji[index];
                 textBlock.Text = nextEmoji;
                 animalEmoji.RemoveAt(index);
+            }
+        }
+
+        //Varibales that live inside the class but inside the method
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock; if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
             }
         }
     }
